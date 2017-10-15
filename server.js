bf23d068072f0cfa0b5e31476dfb2562160e6660
sunnyolives <alfa@krll.com>
2017-10-15 20:32:38 +0200

    rensket notater, virker ikke helt

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 // an express application
+/* Extend the previous endpoint to take an optional url parameter.
 
+if the request contains a parameter then the returned greeting should reflect this. (i.e Hello Christian as apposed to just Hello)  */
+
+const url = require('url');
 const express = require('express' );
 const app = express();
+const bodyParser = require('body-parser');
+let	greetingsArray = ['God dag', 'Vær hilset', 'Velkommen'];
 
-app.use(express.static('public'));
+app.set('port', (process.env.PORT || 8080));
 
-app.listen(8080, function () {
-  console.log('Arbeider hardt, via port 8080!')
-})
+app.listen(app.get('port'), function () {
+  console.log('Node express app started at port ' + app.get('port'));
+});
 
+app.use(express.static('public'));r
+
+app.get('/hilsen', function (req, res) {
+	const tilfeldigIndeks = Math.floor(Math.random()*greetingsArray.length);
+	let name = req.query.name;
+	let greeting = greetingsArray[tilfeldigIndeks];
+	res.send(greeting, greetingsArray[tilfeldigIndeks]);
+});
